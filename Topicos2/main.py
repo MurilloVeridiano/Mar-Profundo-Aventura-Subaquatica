af5bcb7866408ad020bc767d48fb4fe3ba6e7295
Uri <129327609+Biguri@users.noreply.github.com>
2024-03-29 15:19:48 -0400

    Update main.py

--- Topicos2/main.py
+++ Topicos2/main.py
@@ -1,4 +1,4 @@
-import pygame, time
+import pygame
 
 # definindo as cores
 PRETO = (0, 0, 0)
@@ -6,12 +6,13 @@ AMARELO = (255, 255, 0)
 VERMELHO = (255, 0, 0)
 VERDE = (0, 255, 0)
 AZUL = (0, 0, 255)
+BRANCO = (255, 255, 255)
 
 # definindo outras constantes do jogo
 LARGURAJANELA = 500
 ALTURAJANELA = 400
 
-# definindo a função mover()
+# definindo a função mover(), que registra a posição de uma figura
 def mover(figura, dim_janela):
     borda_esquerda = 0
     borda_superior = 0
@@ -26,26 +27,31 @@ def mover(figura, dim_janela):
     figura['objRect'].x += figura['vel'][0]
     figura['objRect'].y += figura['vel'][1]
 
-# inicializando módulos de pygame
+# inicializando pygame
 pygame.init()
+
+# criando um objeto pygame.time.Clock
+relogio = pygame.time.Clock()
 
 # criando a janela
 janela = pygame.display.set_mode((LARGURAJANELA, ALTURAJANELA))
-pygame.display.set_caption('Animação')
+pygame.display.set_caption('Colisão')
 
-# criando as figuras
-f1 = {'objRect': pygame.Rect(300, 80, 40, 80), 'cor': VERMELHO, 'vel': [0,-5], 'forma': 'ELIPSE'}
-f2 = {'objRect': pygame.Rect(200, 200, 20, 20), 'cor': VERDE, 'vel': [5,5], 'forma': 'ELIPSE'}
-f3 = {'objRect': pygame.Rect(100, 150, 60, 60), 'cor': AZUL, 'vel': [-5,5], 'forma': 'RETANGULO'}
-f4 = {'objRect': pygame.Rect(200, 150, 80, 40), 'cor': AMARELO, 'vel': [5,0], 'forma': 'RETANGULO'}
+# criando os blocos e colocando-os em uma lista
+b1 = {'objRect': pygame.Rect(375, 80, 40, 40), 'cor': VERMELHO, 'vel': [0,2]}
+b2 = {'objRect': pygame.Rect(175, 200, 40, 40), 'cor': VERDE, 'vel': [0,-3]}
+b3 = {'objRect': pygame.Rect(275, 150, 40, 40), 'cor': AMARELO, 'vel': [0,-1]}
+b4 = {'objRect': pygame.Rect(75, 150, 40, 40), 'cor': AZUL, 'vel': [0,4]}
+blocos = [b1, b2, b3, b4]
 
-figuras = [f1, f2, f3, f4]
+# criando a bola
+bola = {'objRect': pygame.Rect(270, 330, 30, 30), 'cor': BRANCO, 'vel': [3,3]}
 
 deve_continuar = True
 
 # loop do jogo
 while deve_continuar:
-    #checando se ocorreu um evento QUIT
+    # checando se ocorreu um evento QUIT
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             deve_continuar = False
@@ -53,32 +59,27 @@ while deve_continuar:
     # preenchendo o fundo com a cor preta
     janela.fill(PRETO)
 
-    for figura in figuras:
-        # reposicionando a figura
-        mover(figura,(LARGURAJANELA, ALTURAJANELA))
+    for bloco in blocos:
+        # reposicionando o bloco
+        mover(bloco, (LARGURAJANELA,ALTURAJANELA))
 
-        # desenhando a figura na janela
-        if figura['forma'] == 'RETANGULO':
-            pygame.draw.rect(janela, figura['cor'], figura['objRect'])
-        elif figura['forma'] == 'ELIPSE':
-            pygame.draw.ellipse(janela, figura['cor'], figura['objRect'])
-            
-    # atualizando na tela tudo o que foi desenhado
+        # desenhando o bloco na janela
+        pygame.draw.rect(janela, bloco['cor'], bloco['objRect'])
+
+        # mudando a cor da bola caso colida com algum bloco
+        mudarCor = bola['objRect'].colliderect(bloco['objRect'])
+        if mudarCor:
+            bola['cor'] = bloco['cor']
+
+    # reposicionando e desenha a bola
+    mover(bola, (LARGURAJANELA, ALTURAJANELA))
+    pygame.draw.ellipse(janela, bola['cor'], bola['objRect'])
+
+    # mostrando na tela tudo o que foi desenhado
     pygame.display.update()
-    # esperando 0.02 segundos
-    time.sleep(0.02)
+
+    # limitando a 40 quadros por segundo
+    relogio.tick(40)
 
 # encerrando módulos de Pygame
 pygame.quit()
-
-
-
-
-
-
-
-
-
-
-
-
