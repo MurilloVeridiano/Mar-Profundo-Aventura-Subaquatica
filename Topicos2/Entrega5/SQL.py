4f5a7d6c020c26876217b96747ce97b2299cc46d
MurilloVeridiano <115113083+MurilloVeridiano@users.noreply.github.com>
2024-06-05 00:05:03 -0400

    Update SQL.py

--- Topicos2/Entrega5/SQL.py
+++ Topicos2/Entrega5/SQL.py
@@ -1,4 +1,3 @@
-#SQL.py
 from getpass import getpass
 from mysql.connector import connect, Error
 
@@ -7,8 +6,8 @@ def run_query(hosts, port, db_query):
         result_all = []
         for host in hosts:
             print(f'Host: {host}')
-            username = "user_game"#input("Enter username: ")
-            password = "123"#getpass("Enter password: ")
+            username = "user_game"  # input("Enter username: ")
+            password = "123"  # getpass("Enter password: ")
             print(f'Debug: username = {username}, password = {password}')  # Apenas para debug
             try:
                 with connect(host=host, port=port, user=username, password=password) as connection:
@@ -31,11 +30,8 @@ def run_query(hosts, port, db_query):
     except Error as e:
         print(f"Error running query: {e}")
 
-if __name__ == '__main__':
+def update_hi_score(new_hi_score):
     hosts = ['localhost']
     port = 3306
-    db_query = 'USE game; SELECT hi_score FROM score WHERE idscore = 1;'
-    res_list = run_query(hosts, port, db_query)
-    print("RESULT ALL")
-    for row in res_list:
-        print(row)
+    db_query = f"USE game; UPDATE score SET hi_score = {new_hi_score} WHERE idscore = 1;"
+    run_query(hosts, port, db_query)
