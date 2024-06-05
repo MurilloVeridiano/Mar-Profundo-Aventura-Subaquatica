87ca3d1ce9102bd6ab8fea8133bf8917155e1f71
MurilloVeridiano <115113083+MurilloVeridiano@users.noreply.github.com>
2024-06-05 00:07:00 -0400

    Update atualizar_tela.py

--- Topicos2/Entrega5/atualizar_tela.py
+++ Topicos2/Entrega5/atualizar_tela.py
@@ -1,7 +1,5 @@
-# atualizar_tela.py
-#from menu import *
 import pygame
-from SQL import run_query
+from SQL import run_query, update_hi_score
 
 def get_hi_score():
     hosts = ['localhost']
@@ -20,21 +18,24 @@ class AtualizarTela:
         self.cenario = cenario
         self.gerenciar_objetos = gerenciar_objetos
         self.checar_colisoes = checar_colisoes
-        self.hi_score = get_hi_score()  
+        self.hi_score = get_hi_score()
 
     def atualizar_tela(self):
         # Limpa a tela
         self.janela.fill((0, 0, 0))
-        # Desenha os cenários
-        for posicao, cenario in zip(self.cenario.posicoes_cenario, self.cenario.cenarios):
-            self.janela.blit(cenario, posicao)
-        # Desenha o jogador e a minhoca
+        # Desenha os cenários e a minhoca
+        self.cenario.desenhar()
+        # Desenha o jogador
         self.janela.blit(self.gerenciar_objetos.jogador['imagem'], self.gerenciar_objetos.jogador['objRect'])
-        self.janela.blit(self.gerenciar_objetos.obj_minhoca['imagem'], self.gerenciar_objetos.obj_minhoca['objRect'])
         # Atualiza a pontuação
         texto_pontuacao = self.fonte_pontos.render(f"PONTUAÇÃO: {self.checar_colisoes.pontos}", True, (255, 255, 255))
-        self.janela.blit(texto_pontuacao, (0,0))
+        self.janela.blit(texto_pontuacao, (0, 0))
         
+        # Verifica se a pontuação atual é maior que o hi_score
+        if self.checar_colisoes.pontos > self.hi_score:
+            self.hi_score = self.checar_colisoes.pontos
+            update_hi_score(self.hi_score)
+
         # Atualiza a pontuação máxima (hi-score)
         texto_hi_score = self.fonte_pontos.render(f'Hi-Score: {self.hi_score}', True, (255, 255, 255))
         self.janela.blit(texto_hi_score, (205, 0))
