4fed03cd39dbfd2d4ddf5e4ddd00f4ce84966b9e
MurilloVeridiano <115113083+MurilloVeridiano@users.noreply.github.com>
2024-06-05 00:05:54 -0400

    Create gerenciar_objetos.py

---
+++ Topicos2/Entrega5/gerenciar_objetos.py
@@ -0,0 +1,17 @@
+import pygame
+from configuracoes import VEL
+
+class GerenciarObjetos:
+    def __init__(self, cenario):
+        # Inicializa jogador
+        self.jogador = {
+            'objRect': pygame.Rect(50, 50, cenario.peixe.get_width(), cenario.peixe.get_height()),
+            'imagem': cenario.peixe,
+            'vel': VEL
+        }
+        # Inicializa minhoca
+        self.obj_minhoca = {
+            'objRect': cenario.obj_rect_minhoca,
+            'imagem': cenario.minhoca,
+            'vel': VEL
+        }
